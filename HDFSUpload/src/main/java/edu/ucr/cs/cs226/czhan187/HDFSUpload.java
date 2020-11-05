d259ef735d25c3f8d233976a25fffead5f6b7bcd
chuliang014 <chuliang014@gmail.com>
2020-11-05 15:15:23 -0800

    update:initial program

---
+++ HDFSUpload/src/main/java/edu/ucr/cs/cs226/czhan187/HDFSUpload.java
@@ -0,0 +1,16 @@
+package edu.ucr.cs.cs226.czhan187;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class HDFSUpload
+{
+
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
