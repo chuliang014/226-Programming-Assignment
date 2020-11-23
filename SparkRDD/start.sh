9e31b6fd34e530b9e1e258c1cdd5cea4e408418c
chuliang014 <chuliang014@gmail.com>
2020-11-23 15:35:41 -0800

    update: update description in start.sh

--- SparkRDD/start.sh
+++ SparkRDD/start.sh
@@ -1,2 +1,4 @@
 mvn package
 spark-submit --master local --class edu.ucr.cs.cs226.czhan187.SparkRDD target/SparkRDD-1.0-SNAPSHOT.jar $1
+#just run the command ./start.sh <path of nasa.tsv>
+#eg. ./start.sh /home/download/nasa.tsv
